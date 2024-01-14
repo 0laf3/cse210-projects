cc66b42b7b602a86c9a64b287308631c5c04df95
0laf3 <capindissa@hotmail.com>
2024-01-14 09:03:45 +0100

    solving prep1

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -4,6 +4,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep1 World!");
+        Console.WriteLine("What is you name first name?");
+        string fistname = Console.ReadLine();
+        Console.WriteLine("What is your last name?");
+        string lastname = Console.ReadLine();
+        Console.WriteLine($"Your name is {lastname}, {fistname} {lastname}.");
     }
 }
