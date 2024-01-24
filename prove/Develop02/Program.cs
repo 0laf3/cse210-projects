a0ac8b89851b8bd4eed6da40705a127e3782dc3e
0laf3 <43992006+0laf3@users.noreply.github.com>
2024-01-24 13:25:30 +0100

    changing imput in line 10

--- prove/Develop02/Program.cs
+++ prove/Develop02/Program.cs
@@ -5,15 +5,10 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Please select one of the fllowing choice");
+        Console.WriteLine("\nPlease select one of the fllowing choice\n");
 
-
-        Console.WriteLine("1. Write");
-        Console.WriteLine("2. Display");
-        Console.WriteLine("3. Load");
-        Console.WriteLine("4. Save");
-        Console.WriteLine("5. Quit");
-        Console.WriteLine("What Would you Like to do? ");
+        Console.WriteLine("1. Write\n5. Quit\n4. Save\n3. Load\n2. Display\n");
+        Console.WriteLine("What Would you Like to do?\n> ");
         string choice = Console.ReadLine();
         while (true)
         {
